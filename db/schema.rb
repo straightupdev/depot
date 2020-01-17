5247cd61e6da09608e039e5bbc611ddc121a12da
Will Conry <willconrydeveloper@gmail.com>
2020-01-17 00:43:55 +0000

    added cart functionality and scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200116235219) do
+ActiveRecord::Schema.define(version: 20200117000245) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
