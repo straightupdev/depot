66e5d835b5cf2ef1e5ebca55e2caf86c48a10728
Will Conry <willconrydeveloper@gmail.com>
2020-01-21 18:38:58 +0000

    added html email views, test cases for action mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -12,7 +12,7 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.shipped.subject
   #
-  def shipped
+  def shipped(order)
     @order = order
     mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
