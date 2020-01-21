d2f28b3d993f2317f13e9fc22dc08d5cbe0970cb
Will Conry <willconrydeveloper@gmail.com>
2020-01-21 17:22:02 +0000

    added mail server details to environment config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address:"smtp.gmail.com",
+        port: 587,
+        domain: "straightupdev.com",
+        authentication: "plain",
+        user_name: "willconrydeveloper@gmail.com",
+        password: "VMdepot@6556",
+        enable_starttls_auto: true
+    }
+end
