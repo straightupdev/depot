d8528fb8c6019bc00cc6bac1329c1cb745a5768b
Will Conry <willconrydeveloper@gmail.com>
2020-01-23 21:55:59 +0000

    finished translations, added language selector on home page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,8 +4,12 @@ class StoreController < ApplicationController
   before_action :set_cart
   
   def index
-    @products = Product.all
-    #@products = Product.order(:title)
+    if params[:set_locale]
+      redirect_to store_index_url(locale: params[:set_locale])
+    else
+      @products = Product.all
+      #@products = Product.order(:title)
+    end
   end
   
 end
